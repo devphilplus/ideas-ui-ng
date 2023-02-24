7fc07329850efd17ea88aa3b8db9ba5eace32019
ferd <beowulf1416@gmail.com>
2023-02-24 21:13:41 +0800

    modify sign in component

--- ideas/src/app/components/sign-in/sign-in.component.ts
+++ ideas/src/app/components/sign-in/sign-in.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ApiResponse } from 'src/app/classes/api-response';
+import { AuthService } from 'src/app/services/auth.service';
 import { TitleService } from 'src/app/services/title.service';
 
 @Component({
@@ -8,6 +11,8 @@ import { TitleService } from 'src/app/services/title.service';
   styleUrls: ['./sign-in.component.scss']
 })
 export class SignInComponent implements OnInit {
+
+  enabled = true;
 
   signinForm = new FormGroup({
     email: new FormControl('', [
@@ -20,7 +25,9 @@ export class SignInComponent implements OnInit {
   });
 
   constructor(
-    private title: TitleService
+    private title: TitleService,
+    private router: Router,
+    private auth_service: AuthService
   ) {
     this.title.set_title('Sign In');
   }
@@ -30,5 +37,11 @@ export class SignInComponent implements OnInit {
 
   signin(): void {
     console.log('SignInComponent::signin()');
+
+    this.enabled = false;
+    this.auth_service.signin(
+      this.signinForm.get('email')?.value || '',
+      this.signinForm.get('pw')?.value || ''
+    );
   }
 }
