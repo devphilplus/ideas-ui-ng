f96da60b0b0d59d00616356c7c3728f58656197e
ferd <beowulf1416@gmail.com>
2023-02-27 21:30:35 +0800

    add client component. modify continue sign-up component.

--- ideas/src/app/app-routing.module.ts
+++ ideas/src/app/app-routing.module.ts
@@ -5,11 +5,16 @@ import { NotFoundComponent } from './components/not-found/not-found.component';
 import { SignInComponent } from './components/sign-in/sign-in.component';
 import { SignUpModule } from './modules/sign-up/sign-up.module';
 import { SignOutComponent } from './components/sign-out/sign-out.component';
+import { ForgotPwModule } from './modules/forgot-pw/forgot-pw.module';
 
 const routes: Routes = [
   {
     path: "sign-up",
     loadChildren: () => import('./modules/sign-up/sign-up.module').then(m => SignUpModule)
+  },
+  {
+    path: 'forgot-pw',
+    loadChildren: () => import('./modules/forgot-pw/forgot-pw.module').then(m => ForgotPwModule)
   },
   {
     path: "admin",
--- ideas/src/app/modules/admin/modules/clients/clients.module.ts
+++ ideas/src/app/modules/admin/modules/clients/clients.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MaterialModule } from 'src/app/material/material.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { ClientsRoutingModule } from './clients-routing.module';
 import { HomeComponent } from './components/home/home.component';
 import { ClientComponent } from './components/client/client.component';
@@ -13,6 +16,9 @@ import { ClientComponent } from './components/client/client.component';
   ],
   imports: [
     CommonModule,
+    MaterialModule,
+    FormsModule,
+    ReactiveFormsModule,
     ClientsRoutingModule
   ]
 })
