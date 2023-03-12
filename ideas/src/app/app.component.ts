166bce7c583305b589092e047c978a80fcf93291
ferd <beowulf1416@gmail.com>
2023-03-12 15:48:13 +0800

    modify notifications panel

--- ideas/src/app/app.component.ts
+++ ideas/src/app/app.component.ts
@@ -4,7 +4,7 @@ import { environment } from 'src/environments/environment';
 import { UserService } from './services/user.service';
 import { User } from './classes/user';
 import { Observable } from 'rxjs';
-import { MessageService } from './services/message.service';
+import { Message, MessageService } from './services/message.service';
 
 @Component({
   selector: 'app-root',
@@ -15,6 +15,7 @@ export class AppComponent {
 
   app_name = environment.app_name;
   user$: Observable<User> = new Observable<User>();
+  messages: Array<Message> = [];
 
   constructor(
     private title: TitleService,
@@ -24,5 +25,9 @@ export class AppComponent {
     this.title.set_title('Welcome');
 
     this.user$ = this.user_service.user$
+
+    this.msg_service.message$.subscribe((r: Message) => {
+      this.messages.push(r);
+    });
   }
 }
