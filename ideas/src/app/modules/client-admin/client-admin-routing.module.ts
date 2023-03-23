35b3573d897a10e7f062d665bbf958e490b65bff
ferd <beowulf1416@gmail.com>
2023-03-23 09:56:57 +0800

    add client roles

--- ideas/src/app/modules/client-admin/client-admin-routing.module.ts
+++ ideas/src/app/modules/client-admin/client-admin-routing.module.ts
@@ -2,8 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { MembersComponent } from './components/members/members.component';
+import { RolesComponent } from './components/roles/roles.component';
 
 const routes: Routes = [
+  {
+    path: 'roles',
+    component: RolesComponent
+  },
   {
     path: 'members',
     component: MembersComponent
--- ideas/src/app/modules/client-admin/client-admin.module.ts
+++ ideas/src/app/modules/client-admin/client-admin.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { ClientAdminRoutingModule } from './client-admin-routing.module';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { MembersComponent } from './components/members/members.component';
+import { RolesComponent } from './components/roles/roles.component';
 
 
 @NgModule({
   declarations: [
     DashboardComponent,
-    MembersComponent
+    MembersComponent,
+    RolesComponent
   ],
   imports: [
     CommonModule,
