bdc5fc75a315750cb362fd5fff3624ed4ee02a52
ferd <beowulf1416@gmail.com>
2023-03-30 05:52:43 +0800

    modify tenants dashboard

--- ideas/src/app/modules/admin/modules/users/users.module.ts
+++ ideas/src/app/modules/admin/modules/users/users.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { UsersRoutingModule } from './users-routing.module';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    DashboardComponent
+  ],
   imports: [
     CommonModule,
     UsersRoutingModule
