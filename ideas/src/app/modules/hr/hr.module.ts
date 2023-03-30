7e7e0d2f60bb52fa30178c63d08ed56715c249df
ferd <beowulf1416@gmail.com>
2023-03-30 22:51:52 +0800

    add home and dashboard components

--- ideas/src/app/modules/hr/hr.module.ts
+++ ideas/src/app/modules/hr/hr.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HrRoutingModule } from './hr-routing.module';
+import { HomeComponent } from './components/home/home.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomeComponent,
+    DashboardComponent
+  ],
   imports: [
     CommonModule,
     HrRoutingModule
