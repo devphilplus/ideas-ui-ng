1be44235d049a1ebc16296bccaadda38dcbffd25
ferd <beowulf1416@gmail.com>
2023-03-30 21:29:55 +0800

    modify admin module

--- ideas/src/app/modules/admin/admin-routing.module.ts
+++ ideas/src/app/modules/admin/admin-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
   {
     path: '',
+    component: HomeComponent,
     children: [
       {
         path: '',
--- ideas/src/app/modules/admin/admin.module.ts
+++ ideas/src/app/modules/admin/admin.module.ts
@@ -3,14 +3,20 @@ import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { HomeComponent } from './components/home/home.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MaterialModule } from 'src/app/material/material.module';
 
 
 @NgModule({
   declarations: [
-    DashboardComponent
+    DashboardComponent,
+    HomeComponent
   ],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
+    MaterialModule,
     AdminRoutingModule
   ]
 })
