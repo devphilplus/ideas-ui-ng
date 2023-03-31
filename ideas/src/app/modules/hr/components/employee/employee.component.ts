c0da7f3fac87f3cf7d49e7776b991c9ff2a2c7a4
ferd <beowulf1416@gmail.com>
2023-03-31 16:44:22 +0800

    modify employee and attendance component

--- ideas/src/app/modules/hr/components/employee/employee.component.ts
+++ ideas/src/app/modules/hr/components/employee/employee.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { TitleService } from 'src/app/services/title.service';
 
 @Component({
   selector: 'app-employee',
@@ -7,4 +9,36 @@ import { Component } from '@angular/core';
 })
 export class EmployeeComponent {
 
+  enabled = true;
+  message = '';
+  message_type = 'info';
+
+  employeeForm = new FormGroup({
+    given_name: new FormControl('', [
+      Validators.required
+    ]),
+    middle_name: new FormControl('', [
+    ]),
+    family_name: new FormControl('', [
+      Validators.required
+    ])
+  });
+
+  constructor(
+    private title: TitleService
+  ) {
+    this.title.set_title('Employee');
+  }
+
+  get given_name() {
+    return this.employeeForm.get('given_name');
+  }
+
+  get middle_name() {
+    return this.employeeForm.get('middle_name');
+  }
+
+  get family_name() {
+    return this.employeeForm.get('family_name');
+  }
 }
