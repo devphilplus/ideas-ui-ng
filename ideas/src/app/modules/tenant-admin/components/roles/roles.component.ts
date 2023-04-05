8a7f06d6d81eebc1da5ab46f76f6f534a8fadac8
ferd <beowulf1416@gmail.com>
2023-04-05 18:28:00 +0800

    modify role component

--- ideas/src/app/modules/tenant-admin/components/roles/roles.component.ts
+++ ideas/src/app/modules/tenant-admin/components/roles/roles.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { TitleService } from 'src/app/services/title.service';
 
 @Component({
   selector: 'app-roles',
@@ -7,4 +9,23 @@ import { Component } from '@angular/core';
 })
 export class RolesComponent {
 
+  enabled = true;
+  message = '';
+  message_type = 'info';
+
+  roleForm = new FormGroup({
+    name: new FormControl('', [
+      Validators.required
+    ])
+  });
+
+  constructor(
+    private title: TitleService
+  ) {
+    this.title.set_title('Role');
+  }
+
+  submit(): void {
+    console.log('RolesComponent::submit()');
+  }
 }
