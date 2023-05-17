f39a39ba4078b1af2d52e308240e5b4ced1b7123
ferd <beowulf1416@gmail.com>
2023-05-17 22:31:45 +0800

    modified tenant admin

--- ideas/src/app/modules/tenant-admin/components/organizations/organizations.component.ts
+++ ideas/src/app/modules/tenant-admin/components/organizations/organizations.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { TitleService } from 'src/app/services/title.service';
 
 @Component({
   selector: 'app-organizations',
@@ -7,4 +9,20 @@ import { Component } from '@angular/core';
 })
 export class OrganizationsComponent {
 
+  orgForm = new FormGroup({
+    name: new FormControl('', [
+      Validators.required
+    ]),
+    description: new FormControl('', [])
+  });
+
+  constructor(
+    private title: TitleService
+  ) {
+    this.title.set_title('Organization');
+  }
+
+  submit(): void {
+    console.log('OrganizationsComponent::submit()');
+  }
 }
